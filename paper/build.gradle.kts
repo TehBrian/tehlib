30869f68afc38ec0cfb897aba0024b3b45041cc5
TehBrian <tehbrian@outlook.com>
2021-08-02 12:19:07 -0400

    add corn and paper via api

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
 }
 
 repositories {
+    // for corn itembuilder, remove once its released
+    mavenLocal()
+
     maven {
         url = uri("https://papermc.io/repo/repository/maven-public/")
     }
@@ -10,8 +13,8 @@ repositories {
 
 dependencies {
     api(project(":tehlib-core"))
+    api("broccolai.corn:corn-minecraft-paper:3.0.0-SNAPSHOT")
+    api("cloud.commandframework", "cloud-paper", "1.5.0")
 
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
-
-    implementation("cloud.commandframework", "cloud-paper", "1.5.0")
 }
