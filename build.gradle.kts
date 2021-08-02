2c9975ee88b8f112ba95f5b30dad8726aaa37245
TehBrian <tehbrian@outlook.com>
2021-08-02 12:25:19 -0400

    remove duplicate info in build.gradle.kts
    i may regret this later

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     id("net.kyori.indra.publishing.sonatype") version "2.0.1"
 }
 
-group = "dev.tehbrian"
-version = "0.1.0"
-description = "Some small, helpful utilites."
-
 indraSonatype {
     useAlternateSonatypeOSSHost("s01")
 }
