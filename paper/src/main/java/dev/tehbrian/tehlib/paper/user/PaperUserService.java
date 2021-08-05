e884fc3fa85a9e9cf2b0fd8a3e4ba02144f1a9a4
TehBrian <tehbrian@outlook.com>
2021-08-04 22:15:40 -0400

    docs: fix wrong link

--- paper/src/main/java/dev/tehbrian/tehlib/paper/user/PaperUserService.java
+++ paper/src/main/java/dev/tehbrian/tehlib/paper/user/PaperUserService.java
@@ -1,6 +1,5 @@
 package dev.tehbrian.tehlib.paper.user;
 
-import dev.tehbrian.tehlib.core.user.AbstractUser;
 import dev.tehbrian.tehlib.core.user.AbstractUserService;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -8,14 +7,14 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import java.util.UUID;
 
 /**
- * Stores and retrieves {@link AbstractUser} instances.
+ * Stores and retrieves {@link PaperUser} instances.
  *
  * @param <U> the user type
  */
 public abstract class PaperUserService<U extends PaperUser> extends AbstractUserService<UUID, U> {
 
     /**
-     * Gets the {@code User} associated with that {@code Player's} {@code uuid}.
+     * Gets the {@code User} associated with that {@code Player}'s {@code uuid}.
      *
      * @param player the player
      * @return the {@code User}
