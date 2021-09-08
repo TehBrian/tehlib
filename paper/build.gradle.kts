8f6bb8c4dc21d5c3f33c04ec07505ada145e39b5
TehBrian <tehbrian@protonmail.com>
2021-09-08 15:14:40 -0400

    remove maven local dep

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
 }
 
 repositories {
-    // for corn itembuilder, remove once its released
-    mavenLocal()
-
     maven {
         url = uri("https://papermc.io/repo/repository/maven-public/")
     }
@@ -16,6 +13,6 @@ dependencies {
 
     api("broccolai.corn:corn-minecraft-paper:3.0.0-SNAPSHOT")
     api("cloud.commandframework", "cloud-paper", "1.5.0")
-    
+
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 }
