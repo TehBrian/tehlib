02e1a006177524543efcd5c8c168a263dce99e7d
TehBrian <tehbrian@protonmail.com>
2022-01-31 13:49:36 -0500

    build[core]: remove unused sonatype repo

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     id("tehlib.java-conventions")
 }
 
-repositories {
-    maven("https://oss.sonatype.org/content/repositories/snapshots/") {
-        name = "sonatype-oss-snapshots"
-    }
-}
-
 dependencies {
     api("cloud.commandframework", "cloud-core", "1.6.1")
     api("org.spongepowered:configurate-core:4.1.2")
