761e5e89ca932e3ddd153a23112e16b3df110f5a
TehBrian <tehbrian@protonmail.com>
2022-03-04 16:38:45 -0500

    paper 1.18.1 -> 1.18.2, remove minimessage (now bundled into paper)

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -12,7 +12,5 @@ dependencies {
     api(project(":tehlib-core"))
 
     api("cloud.commandframework", "cloud-paper", "1.6.2")
-    api("net.kyori:adventure-text-minimessage:4.10.0")
-
-    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
