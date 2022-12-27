e76cc48a8ef6ed2b94a521bc019f8d6a90f08fc0
TehBrian <tehbrian@proton.me>
2022-12-27 13:57:35 -0500

    build: remove sonatype publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
-  id("net.kyori.indra.publishing.sonatype") version "3.0.1"
   id("com.github.ben-manes.versions") version "0.44.0"
 }
 
 group = "dev.tehbrian"
 version = "0.4.0"
 description = "Some small, helpful utilities."
-
-indraSonatype {
-  useAlternateSonatypeOSSHost("s01")
-}
