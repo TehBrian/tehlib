a4fe189b266ffede897f96fbffd1721c6a3bead6
TehBrian <tehbrian@proton.me>
2023-09-02 12:46:41 -0400

    version 0.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("com.github.ben-manes.versions") version "0.45.0"
+  id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 group = "dev.tehbrian"
-version = "0.4.2"
+version = "0.5.0"
 description = "Some small, helpful utilities."
